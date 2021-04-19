21285c57272a91da4ce8296745144777679f18b5
Olga Rozhkova <olga.gavrilova.og94@gmail.com>
2021-04-19 17:13:31 +0300

    change name

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AppHeader = ({liked, allPosts}) => {
     return (
         <div className="app-header d-flex">
-            <h1>Ivan Petrychenko</h1>
+            <h1>Olga Rozhkova</h1>
             <h2>{allPosts} записей, из них понравилось {liked}</h2>
         </div>
     )
